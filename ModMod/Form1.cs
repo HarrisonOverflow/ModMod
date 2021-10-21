93ddf54831da455ca8e52e6dd47ce25a9983e087
362011 <362011@CHS-232-16>
2021-10-21 09:44:02 -0500

    Add project files.

---
+++ ModMod/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModMod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int startNum = 1;
+            int endNum = 100;
+            double sum = 0;
+            double allSq = 0;
+
+            
+            for (int i = startNum; i <= endNum; i++ ) 
+            {
+                sum += 1;
+                allSq = Math.Pow(1, 2);
+            }
+            if (Convert.ToInt64(Math.Pow(Math.Pow(sum,2)-allSq,(1.0/3.0)))% 2 == 0)
+            {
+                label1.Text = "Even";
+            }
+        }
+    }
+}
